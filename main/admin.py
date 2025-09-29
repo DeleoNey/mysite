53b420651af7516a93c869588567685182fabd73
Deleon <makslesk64@gmail.com>
2025-09-29 17:24:20 +0300

    Created models for Post and Profile. Added these models to admin. Set Static and Media urs/root in settings.

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile, Post
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name')
+    search_fields = ('name', 'user__username')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('author', 'created_at')
--- main/models.py
+++ main/models.py
@@ -1,3 +1,68 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(default='avatars/default_avatar/default.jpg', upload_to='avatars/',
+                               blank=True,
+                               null=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.user.username)
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
+    def get_absolute_url(self):
+        return reverse("profile-detail", args=[self.id, self.slug])
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='post_images/', blank=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug = base_slug
+            counter = 1
+            while Post.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.title
+
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"slug": self.slug})
